c829276e2b7437c61c4b8904e22adac51ece8a30
Furqan Azhar <furqanazhar92@gmail.com>
2022-07-12 18:18:56 +0500

    Bubble sort algorithm added

---
+++ bubble_sort.py
@@ -0,0 +1,24 @@
+
+
+def bubble_sort(array):
+    steps = 0
+    swapped = False
+    for i in range(len(array)):
+        for j in range(0,(len(array))-i-1):
+            steps+=1
+            if array[j]>array[j+1]:
+                swapped = True
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+        if not swapped:
+            break
+    return array, steps
+
+array = [10,9,8,7,6,5,4,3,2,1]
+size = len(array)
+print("Input : ", array)
+output, steps = bubble_sort(array)
+print("Size of Array : ", size)
+print("Steps Taken : ", steps)
+print("Result : ",output)
