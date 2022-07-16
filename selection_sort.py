a05524a52d96fa723070b19f8cf482cf5fbf7605
Furqan Azhar <furqanazhar92@gmail.com>
2022-07-16 22:41:34 +0500

    Selection sort algorithm added

---
+++ selection_sort.py
@@ -0,0 +1,22 @@
+
+
+def selection_sort(array):
+    steps = 0
+    minimum = 0
+    for i in range(len(array)):
+        minimum = i
+        for j in range(i+1, len(array)):
+            steps+=1
+            if array[minimum]>array[j]:
+                minimum = j
+        array[minimum], array[i] = array[i], array[minimum]
+
+    return array, steps
+
+array = [10,9,8,7,6,5,4,3,2,1]
+size = len(array)
+print("Input : ", array)
+output, steps = selection_sort(array)
+print("Size of Array : ", size)
+print("Steps Taken : ", steps)
+print("Result : ",output)
